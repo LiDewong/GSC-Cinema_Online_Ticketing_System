ca5d01b673db1fde2148a9b28583441b373fdd69
Christina Ku Pei San <u2102789@siswa.um.edu.my>
2022-01-12 11:38:15 +0800

    change main class

--- CinemaSwingTest1/src/cinemaswingtest1/CinemaSwingTest1.java
+++ CinemaSwingTest1/src/cinemaswingtest1/CinemaSwingTest1.java
@@ -1,9 +1,15 @@
 package cinemaswingtest1;
 
+import javax.swing.JFrame;
+
 public class CinemaSwingTest1 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        CustomerOrStaff cos = new CustomerOrStaff();
+        cos.setVisible(true);
+        cos.pack();
+        cos.setLocationRelativeTo(null);
+        cos.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     
 }
