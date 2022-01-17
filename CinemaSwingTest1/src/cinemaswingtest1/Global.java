77cecd947e4b34311cd53aa74c0212f816ad4c1c
kangqiaorou <kanhqiaorou@gmail.com>
2022-01-17 20:20:30 +0800

    nothing

--- CinemaSwingTest1/src/cinemaswingtest1/Global.java
+++ CinemaSwingTest1/src/cinemaswingtest1/Global.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package cinemaswingtest1;
 
-/**
- *
- * @author User
- */
 public class Global {
     public static String customer_email = "";
     public static String seat_no = "";
