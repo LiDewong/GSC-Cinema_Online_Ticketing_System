5e1bd53add4a85571e1b7abc9e462c26ea3e1641
LiDewong <95974704+LiDewong@users.noreply.github.com>
2022-01-17 22:01:27 +0800

    global seat and movie idx

--- CinemaSwingTest1/src/cinemaswingtest1/Global.java
+++ CinemaSwingTest1/src/cinemaswingtest1/Global.java
@@ -13,5 +13,34 @@ public class Global {
     public static String day = "";
     public static String time = "";
     public static String Url=""; 
-    public static List<Integer> booked_seat = new ArrayList<>();
+    public static int movie_idx;
+    public static List<Integer> booked_seat0 = new ArrayList<>();
+    public static List<Integer> booked_seat1 = new ArrayList<>();
+    public static List<Integer> booked_seat2 = new ArrayList<>();
+    public static List<Integer> booked_seat3 = new ArrayList<>();
+    public static List<Integer> booked_seat4 = new ArrayList<>();
+    
+    public static List<List<Integer>> list = new ArrayList<>() {
+        {
+            add(booked_seat0);
+            add(booked_seat1);
+            add(booked_seat2);
+            add(booked_seat3);
+            add(booked_seat4);
+        }
+    };
+    public static List<Integer> booked_seat0L = new ArrayList<>();
+    public static List<Integer> booked_seat1L = new ArrayList<>();
+    public static List<Integer> booked_seat2L = new ArrayList<>();
+    public static List<Integer> booked_seat3L = new ArrayList<>();
+    public static List<Integer> booked_seat4L = new ArrayList<>();
+    public static List<List<Integer>> listL = new ArrayList<>() {
+        {
+            add(booked_seat0L);
+            add(booked_seat1L);
+            add(booked_seat2L);
+            add(booked_seat3L);
+            add(booked_seat4L);
+        }
+    };
 }
