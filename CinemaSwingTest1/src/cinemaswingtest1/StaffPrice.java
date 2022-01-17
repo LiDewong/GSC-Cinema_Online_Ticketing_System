6f182184ca072d8bbf79d32e8042dcdda153b6e3
kangqiaorou <kanhqiaorou@gmail.com>
2022-01-17 22:54:24 +0800

    staff price

--- CinemaSwingTest1/src/cinemaswingtest1/StaffPrice.java
+++ CinemaSwingTest1/src/cinemaswingtest1/StaffPrice.java
@@ -7,6 +7,9 @@ public class StaffPrice extends javax.swing.JFrame {
 
     public StaffPrice() {
         initComponents();
+        txtStudent.setText(Global.ticket_student);
+        txtClassic.setText(Global.ticket_classic);
+        txtPremium.setText(Global.ticket_premium);
     }
 
     @SuppressWarnings("unchecked")
@@ -17,10 +20,10 @@ public class StaffPrice extends javax.swing.JFrame {
         jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
         jLabel10 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
-        jTextField3 = new javax.swing.JTextField();
+        txtStudent = new javax.swing.JTextField();
+        txtClassic = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
-        jTextField1 = new javax.swing.JTextField();
+        txtPremium = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -46,13 +49,13 @@ public class StaffPrice extends javax.swing.JFrame {
         jLabel10.setText("Student");
         getContentPane().add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 230, 90, -1));
 
-        jTextField2.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jTextField2.setText("RM10.00");
-        getContentPane().add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 220, 220, 50));
+        txtStudent.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        txtStudent.setText("RM10.00");
+        getContentPane().add(txtStudent, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 220, 220, 50));
 
-        jTextField3.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jTextField3.setText("RM17.00");
-        getContentPane().add(jTextField3, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 310, 220, 50));
+        txtClassic.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        txtClassic.setText("RM17.00");
+        getContentPane().add(txtClassic, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 310, 220, 50));
 
         jButton1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jButton1.setText("Save");
@@ -63,9 +66,9 @@ public class StaffPrice extends javax.swing.JFrame {
         });
         getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 510, 90, 40));
 
-        jTextField1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jTextField1.setText("RM25.00");
-        getContentPane().add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 400, 220, 50));
+        txtPremium.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        txtPremium.setText("RM25.00");
+        getContentPane().add(txtPremium, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 400, 220, 50));
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/cinemaswingtest1/image/mainmenu/black 2.jpg"))); // NOI18N
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
@@ -74,7 +77,14 @@ public class StaffPrice extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+            Global.ticket_student = txtStudent.getText();
+            Global.ticket_classic = txtClassic.getText();
+            Global.ticket_premium = txtPremium.getText();
+
         JOptionPane.showMessageDialog(this, "Save Successfully");
+        StaffMainMenu c = new StaffMainMenu();
+        c.show();
+        this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
@@ -119,8 +129,8 @@ public class StaffPrice extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel9;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField txtClassic;
+    private javax.swing.JTextField txtPremium;
+    private javax.swing.JTextField txtStudent;
     // End of variables declaration//GEN-END:variables
 }
